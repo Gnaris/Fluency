8c0e0e3e8606635e2c82c0423fd52187d4aa9808
Gnaris <chen.boris@hotmail.fr>
2022-05-31 17:05:15 +0200

    couleur en-tête

--- src/components/atomes/Fluency.tsx
+++ src/components/atomes/Fluency.tsx
@@ -4,7 +4,7 @@ import "./style.css";
 const Fluency : FunctionComponent = () => {
     return (
         <div className='fluency'>
-            <h1>FluencySs</h1>
+            <h1>Fluency</h1>
         </div>
     );
 };
